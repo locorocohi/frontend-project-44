2ff04abc5dafa413101d0438a5257b95bb69cc5c
Artem Rudov <dovakin2281337@gmail.com>
2024-02-11 17:25:11 +0300

    replace game in /src

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,39 +1,4 @@
 #!/usr/bin/env node
 
-console.log('Welcome to the Brain Games!')
-
-import input from '../src/cli.js'
-console.log("What's your name?")
-const userName = input()
-console.log(`Hello, ${userName}!`)
-
-console.log('Answer "yes" if the number is even, otherwise answer "no".')
-
-
-let correctAnswer = 0
-let temp = ''
-let numbers = [13, 4, 16]
-for(let number of numbers) {
-    console.log(`Question: ${number}`)
-
-    if (number % 2 === 0) {
-    temp = 'yes'
-    } else {
-    temp ='no'
-    }
-
-let answer = input()
-
-    if (answer === temp) {
-    correctAnswer = correctAnswer + 1
-    console.log('Correct!')
-    } else {
-    console.log(`"${answer}" is wrong answer ;(. Correct answer was ${temp}.`)
-    console.log(`Let's try again, ${userName}!`)
-    break
-}
-}
-
-if (correctAnswer === 3) {
-    console.log(`Congratulations, ${userName}!`)
-}
+import game from '../src/even-game.js'
+game()
---
+++ src/even-game.js
@@ -0,0 +1,46 @@
+import readlineSync from 'readline-sync';
+
+const input = () => {
+const name = readlineSync.question('Your answer: ');
+return name
+}
+
+const game = () => {
+
+console.log('Welcome to the Brain Games!')
+console.log("What's your name?")
+const userName = input()
+console.log(`Hello, ${userName}!`)
+
+console.log('Answer "yes" if the number is even, otherwise answer "no".')
+
+
+let correctAnswer = 0
+let temp = ''
+let numbers = [13, 4, 16]
+for(let number of numbers) {
+    console.log(`Question: ${number}`)
+
+    if (number % 2 === 0) {
+    temp = 'yes'
+    } else {
+    temp ='no'
+    }
+
+let answer = input()
+
+    if (answer === temp) {
+    correctAnswer = correctAnswer + 1
+    console.log('Correct!')
+    } else {
+    console.log(`"${answer}" is wrong answer ;(. Correct answer was ${temp}.`)
+    console.log(`Let's try again, ${userName}!`)
+    break
+}
+}
+
+if (correctAnswer === 3) {
+    console.log(`Congratulations, ${userName}!`)
+}
+}
+export default game
