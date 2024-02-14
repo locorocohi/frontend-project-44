34dc695f24a548edce8716a75e91220ddc3f8994
Artem Rudov <dovakin2281337@gmail.com>
2024-02-14 16:27:26 +0300

    fix brain-games

--- src/cli.js
+++ src/cli.js
@@ -1,6 +1,7 @@
 import readlineSync from 'readline-sync';
 
 export default () => {
-const name = readlineSync.question('Your answer: ');
-return name
+console.log('Welcome to the Brain Games!');
+const userName = readlineSync.question('May I have your name? ');
+console.log(`Hello, ${userName}!`);
 }
