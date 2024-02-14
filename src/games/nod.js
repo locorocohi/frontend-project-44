2a9d903e1b0cce2a3058231ad20c5009cb71a67f
Artem Rudov <dovakin2281337@gmail.com>
2024-02-14 17:14:44 +0300

    fix GCD

--- src/games/nod.js
+++ src/games/nod.js
@@ -5,7 +5,10 @@ const desc = 'Find the greatest common divisor of given numbers.';
 
 const getQA = () => {
   const firstArgument = getRndInt(1, 100);
-  const secondArgument = getRndInt(1, 100);
+  let secondArgument = getRndInt(1, 100);
+  if (secondArgument === firstArgument) {
+    secondArgument = getRndInt(1, 100)
+  }
   const question = `${firstArgument} ${secondArgument}`;
   const correctAnswer = String(nod(firstArgument, secondArgument));
 
